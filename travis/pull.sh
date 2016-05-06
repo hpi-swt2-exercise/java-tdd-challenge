f4f18690b9e7d09f80a4695ad9a083aeb8535534
Arian Treffer <derari@web.de>
2016-05-06 12:47:59 +0200

    encrypted features

--- travis/pull.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/expect -f
-spawn git pull -X theirs --no-edit "git@github.com:hpi-swt2-exercise/java-tdd-challenge-tasks.git" master
-expect "Enter passphrase for key '/home/travis/.ssh/id_rsa'"
-send "\n";
-interact
--- travis/ssh_add.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/expect -f
-spawn ssh-add /home/travis/.ssh/id_rsa
-expect "Enter passphrase for /home/travis/.ssh/id_rsa:"
-send "\n";
-interact
